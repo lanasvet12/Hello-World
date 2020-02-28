c15474f7736d094eb064e907b6c7a7f01e29412c
Lana <44567161+lanasvet12@users.noreply.github.com>
2020-02-28 11:58:34 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def salvador():
     return 'Hello World!'
+
+@app.route("/about)
+def about():
+    return render_template("about.html")
