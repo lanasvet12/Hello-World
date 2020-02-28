1081372eb0b68c014d5eed14344914ee79c25b05
Lana <44567161+lanasvet12@users.noreply.github.com>
2020-02-28 11:16:28 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/about)
+def about():
+    return render_template("about.html")
+
 @app.route('/salvador')
 def salvador():
     return 'Hello World!'
